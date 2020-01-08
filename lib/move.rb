05104bb18408d3f03f45e1499fc148101ccda4c0
Nicholas Echevarria <nick.echev@gmail.com>
2020-01-08 23:08:36 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -14,5 +14,5 @@ def move(board, user_input, character = "X")
   def update_array_at_with(board, user_input, character)
       board[user_input] = character
   end
-  update_array_at_with(board, user_input, character = "X")
+  update_array_at_with(board, user_input, character)
 end
